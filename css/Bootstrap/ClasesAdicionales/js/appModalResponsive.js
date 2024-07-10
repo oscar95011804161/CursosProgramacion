09942050757a258ec0d205c5d1970eb6a61ff64c
oscar95011804161 <oscar_vanegas1@hotmail.com>
2024-07-10 11:10:20 -0500

    bootstrap

---
+++ css/Bootstrap/ClasesAdicionales/js/appModalResponsive.js
@@ -0,0 +1,17 @@
+new DataTable('#example1', {
+    responsive: {
+        details: {
+            display: DataTable.Responsive.display.modal({
+                header: function (row) {
+                    var data = row.data();
+                    return 'Details for ' + data[0] + ' ' + data[1];
+                }
+            }),
+            renderer: DataTable.Responsive.renderer.tableAll({
+                tableClass: 'table'
+            })
+        }
+    }
+});
+
+
